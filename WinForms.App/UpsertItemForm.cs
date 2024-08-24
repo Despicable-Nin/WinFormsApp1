84ad45130cfc1021b10c590dbad3d9e53ddb0154
Ninism <nin.alamo@outlook.com>
2024-08-25 07:46:53 +0800

    Initial commit

---
+++ WinForms.App/UpsertItemForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms.App
+{
+    public partial class UpsertItemForm : Form
+    {
+        public UpsertItemForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button3_Click_2(object sender, EventArgs e) => Close();
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show(Text, "Are you sure?", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                return;
+            }
+
+            Service.State.Items.Add(new Service.Item
+            {
+                Name = textBox1.Text,
+                Code = textBox2.Text,
+                Price = decimal.Parse(textBox3.Text),
+                Description = textBox4.Text
+            });
+
+            this.Close();
+        }
+    }
+}
